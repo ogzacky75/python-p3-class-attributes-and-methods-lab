b58daff6a18c3eab5308888e1a59f6c15760291d
ogzacky75 <ogzacky75@gmail.com>
2025-09-02 17:14:52 +0300

    fixing error

--- lib/song.py
+++ lib/song.py
@@ -1,6 +1,7 @@
 class Song:
+
     count = 0
-    artist = []
+    artists = []
     genres = []
     genre_count = {}
     artist_count = {}
@@ -9,21 +10,17 @@ class Song:
         self.name = name
         self.artist = artist
         self.genre = genre
-        
+
+
         Song.add_song_to_count()
-        Song.add_to_artist(artist)
-        Song.add_to_genre(genre)
-        Song.add_to_artist_count(artist)
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
         Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
 
     @classmethod
     def add_song_to_count(cls):
         cls.count += 1
-
-    @classmethod
-    def add_to_artist(cls, artist):
-        if artist not in cls.artist:
-            cls.artist.append(artist)
 
     @classmethod
     def add_to_genres(cls, genre):
@@ -31,9 +28,14 @@ class Song:
             cls.genres.append(genre)
 
     @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
     def add_to_genre_count(cls, genre):
         cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
 
     @classmethod
     def add_to_artist_count(cls, artist):
-       cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
